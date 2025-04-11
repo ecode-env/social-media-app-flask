26c47fa52da784aa9c50ef139c6d757790026f3c
eyobScript <eyobbmulugeta@gmail.com>
2025-04-12 02:19:24 +0300

    add database url from env file

--- social_media_backend/app/config.py
+++ social_media_backend/app/config.py
@@ -4,6 +4,6 @@ from os import environ
 load_dotenv()
 
 class Config:
-    SECRET_KEY = environ.get("SECRET_KEY","your-secret-key")
-    DATABASE_URL = environ.get("DATABASE_URL","sqlite:///instance/social_media.db")
+    SECRET_KEY = environ.get("SECRET_KEY","eyob-mulugeta")
+    DATABASE_URL = environ.get("DATABASE_URL","sqlite:///social_media.db")
     JWT_SECRET_KEY = environ.get("JWT_SECRET_KEY","your-jwt-secret")

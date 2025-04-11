533e06543ad1461f5f0f0407f24359718a84f07d
eyobScript <eyobbmulugeta@gmail.com>
2025-04-11 04:19:23 +0300

    create db

--- social_media_backend/app/config.py
+++ social_media_backend/app/config.py
@@ -1,3 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
 
-db = SQLAlchemy()
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///social_media.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'your-jwt-secret')

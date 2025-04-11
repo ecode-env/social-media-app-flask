41ae0456060786d15a66a361dd5946f6ce51017e
eyobScript <eyobbmulugeta@gmail.com>
2025-04-11 04:19:17 +0300

    create db

--- social_media_backend/app/__init__.py
+++ social_media_backend/app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
-from .config import db
+from .extensions import db
+from .config import Config
+from .routes.auth import auth_bp
 
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
 
-    app.config(db)
+    db.init_app(app)
 
+    # Register blueprints
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     with app.app_context():
         db.create_all()

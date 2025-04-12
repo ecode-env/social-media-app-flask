c0af9df9344036bbe9049a51339bc1a52007ae10
eyobScript <eyobbmulugeta@gmail.com>
2025-04-12 10:13:34 +0300

    initial all extentions

--- social_media_backend/app/__init__.py
+++ social_media_backend/app/__init__.py
@@ -6,5 +6,10 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    db.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
+    mail.init_app(app)
+    cors.init_app(app)
 
     return app

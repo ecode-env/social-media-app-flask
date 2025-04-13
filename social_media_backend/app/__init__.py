bc184634a40da70c7a3c7321a3cfdbbf1fe5a208
eyobScript <eyobbmulugeta@gmail.com>
2025-04-13 23:12:48 +0300

    comment

--- social_media_backend/app/__init__.py
+++ social_media_backend/app/__init__.py
@@ -7,6 +7,8 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+
+    # Initialize extensions
     db.init_app(app)
     jwt.init_app(app)
     migrate.init_app(app, db)

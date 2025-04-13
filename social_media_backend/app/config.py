c0c9f776939d42f4e53010a1977b8e06af5ec5c1
eyobScript <eyobbmulugeta@gmail.com>
2025-04-13 23:33:38 +0300

    flask

--- social_media_backend/app/config.py
+++ social_media_backend/app/config.py
@@ -4,10 +4,6 @@ from os import environ
 load_dotenv()
 
 class Config:
-    SECRET_KEY = environ.get("SECRET_KEY","eyob-mulugeta")
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI","sqlite:///social_media.db")
-    JWT_SECRET_KEY = environ.get("JWT_SECRET_KEY","your-jwt-secret")
-
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI", "sqlite:///social_media.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = environ.get("SECRET_KEY", "fallback-secret-key")

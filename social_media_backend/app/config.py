dd3ad5680f7eb7f4f37e1833f3e6fb1101091d2a
eyobScript <eyobbmulugeta@gmail.com>
2025-04-13 23:33:07 +0300

    flask

--- social_media_backend/app/config.py
+++ social_media_backend/app/config.py
@@ -7,3 +7,5 @@ class Config:
     SECRET_KEY = environ.get("SECRET_KEY","eyob-mulugeta")
     SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI","sqlite:///social_media.db")
     JWT_SECRET_KEY = environ.get("JWT_SECRET_KEY","your-jwt-secret")
+
+    JWT_SECRET_KEY = environ.get("JWT_SECRET_KEY", "fallback-jwt-secret")

05bbc9d1e66a9573fc41a00e2b1f8b6d7030937b
eyobScript <eyobbmulugeta@gmail.com>
2025-04-14 16:50:39 +0300

    import models,librarys, and methods

--- social_media_backend/app/routes/auth.py
+++ social_media_backend/app/routes/auth.py
@@ -1,5 +1,12 @@
-from flask import Blueprint
-from ..utils import auth
+from datetime import datetime, timezone
+from email_validator import validate_email, EmailNotValidError
+from ..utils.auth import hash_password,generate_token, verify_password
+from validators import url as validate_url
+from flask import Blueprint,jsonify,request
+from ..models.user import User, UserRoleEnum
+from ..extensions import db
+import re
+
 
 
 auth_bp = Blueprint('auth', __name__)

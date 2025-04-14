1cde9f41dad668962ad811b5aaba3720e9cb95c6
eyobScript <eyobbmulugeta@gmail.com>
2025-04-14 13:48:19 +0300

    the method that hash password

--- social_media_backend/app/utils/auth.py
+++ social_media_backend/app/utils/auth.py
@@ -1,4 +1,8 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_jwt_extended import create_access_token
 from datetime import timedelta
+
 def hash_password(password: str) -> str:
+
+    return generate_password_hash(password)
+

a4b8046dd8f7b53581207f8ef1f71457fba47e01
eyobScript <eyobbmulugeta@gmail.com>
2025-04-15 01:08:05 +0300

    back_populates

--- social_media_backend/app/routes/post.py
+++ social_media_backend/app/routes/post.py
@@ -1,5 +1,9 @@
 from datetime import datetime, timezone
 from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from sqlalchemy.exc import SQLAlchemyError
+
+from ..models.like import Like
 from ..models.post import Post
 from ..models.user import User
 from ..extensions import db

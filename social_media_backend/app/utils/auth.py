cfb80ed018ab76657be020f470fe7d81048aabed
eyobScript <eyobbmulugeta@gmail.com>
2025-04-14 13:48:43 +0300

    to verify password

--- social_media_backend/app/utils/auth.py
+++ social_media_backend/app/utils/auth.py
@@ -6,3 +6,7 @@ def hash_password(password: str) -> str:
 
     return generate_password_hash(password)
 
+def verify_password(password: str, hashed: str) -> bool:
+
+    return check_password_hash(hashed, password)
+

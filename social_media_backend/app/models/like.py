64fa1632799fd66492eb2c5dcf57816e27f5e50f
eyobScript <eyobbmulugeta@gmail.com>
2025-04-14 16:58:49 +0300

    updated value

--- social_media_backend/app/models/like.py
+++ social_media_backend/app/models/like.py
@@ -8,5 +8,5 @@ class Like(db.Model):
     post_id: db.Mapped[int] = db.mapped_column(db.ForeignKey('post.id'), primary_key=True)
     created_at: db.Mapped[datetime] = db.mapped_column(db.DateTime, default=datetime.now(timezone.utc))
 
-    user = db.relationship('User', backref='likes')
+    user = db.relationship('User', back_populates='likes')
     post = db.relationship('Post', backref='likes')

fcbcf5dbdd5e816edbeab6815aa059c846288fa0
eyobScript <eyobbmulugeta@gmail.com>
2025-04-15 01:05:33 +0300

    password

--- social_media_backend/app/utils/auth.py
+++ social_media_backend/app/utils/auth.py
@@ -3,7 +3,6 @@ from flask_jwt_extended import create_access_token
 from datetime import timedelta
 
 def hash_password(password: str) -> str:
-
     return generate_password_hash(password)
 
 def verify_password(password: str, hashed: str) -> bool:

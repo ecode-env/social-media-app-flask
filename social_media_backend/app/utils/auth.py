39515c0b963fa39a5724f163703d10d1c624fd9e
eyobScript <eyobbmulugeta@gmail.com>
2025-04-14 13:48:55 +0300

    to generate token

--- social_media_backend/app/utils/auth.py
+++ social_media_backend/app/utils/auth.py
@@ -10,3 +10,7 @@ def verify_password(password: str, hashed: str) -> bool:
 
     return check_password_hash(hashed, password)
 
+def generate_token(user_id: int) -> str:
+
+    expires = timedelta(hours=24)
+    return create_access_token(identity=user_id, expires_delta=expires)

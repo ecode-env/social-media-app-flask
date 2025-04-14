37c87a2ccb960a02e3214b6fc52c62b08b1bbc44
eyobScript <eyobbmulugeta@gmail.com>
2025-04-15 01:05:40 +0300

    password

--- social_media_backend/app/utils/auth.py
+++ social_media_backend/app/utils/auth.py
@@ -8,7 +8,6 @@ def hash_password(password: str) -> str:
 def verify_password(password: str, hashed: str) -> bool:
     return check_password_hash(hashed, password)
 
-def generate_token(user_id: int) -> str:
-
+def generate_token(user_id: str) -> str:
     expires = timedelta(hours=24)
     return create_access_token(identity=user_id, expires_delta=expires)

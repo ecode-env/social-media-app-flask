de66cac92260ec32935998c599c0928fe0293d15
eyobScript <eyobbmulugeta@gmail.com>
2025-04-14 03:51:57 +0300

    likes model

--- social_media_backend/app/models/like.py
+++ social_media_backend/app/models/like.py
@@ -1,3 +1,12 @@
 from ..extensions import db
 from datetime import timezone, datetime
 
+class Like(db.Model):
+    __tablename__ = "like"
+
+    user_id: db.Mapped[int] = db.mapped_column(db.ForeignKey('user.id'), primary_key=True)
+    post_id: db.Mapped[int] = db.mapped_column(db.ForeignKey('post.id'), primary_key=True)
+    created_at: db.Mapped[datetime] = db.mapped_column(db.DateTime, default=datetime.now(timezone.utc))
+
+    user = db.relationship('User', backref='likes')
+    post = db.relationship('Post', backref='likes')

62bb24209123186b186f28decbcac9188d7911c6
eyobScript <eyobbmulugeta@gmail.com>
2025-04-16 01:15:56 +0300

    to get media type from uploaded file

--- social_media_backend/app/utils/helpers.py
+++ social_media_backend/app/utils/helpers.py
@@ -4,3 +4,11 @@ def allowed_file(filename):
     return ('.' in filename
             and filename.rsplit('.', 1)[1].lower()
             in {'jpg', 'jpeg', 'png', 'gif', 'mp4', 'avi', 'mov'})
+
+def get_media_type(filename):
+    ext = os.path.splitext(filename)[1].lower()
+    if ext in ['.jpg', '.jpeg', '.png', '.gif']:
+        return 'image'
+    elif ext in ['.mp4', '.avi', '.mov']:
+        return 'video'
+    return None

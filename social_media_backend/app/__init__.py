e1edc052eda200ee27cbd124f1e6bcfe63c98e98
eyobScript <eyobbmulugeta@gmail.com>
2025-04-15 18:10:01 +0300

    import admin blueprint

--- social_media_backend/app/__init__.py
+++ social_media_backend/app/__init__.py
@@ -6,6 +6,7 @@ from .routes.user import users_bp
 from .routes.message import messages_db
 from .routes.post import posts_bp
 from .routes.comment import comments_bp
+from .routes.admin import admin_bp
 from .error_handlers.auth_errors import register_auth_error_handlers
 
 

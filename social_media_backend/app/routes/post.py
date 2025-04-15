c3a903fa20f788cd8e867a4b1987cdf5b179be52
eyobScript <eyobbmulugeta@gmail.com>
2025-04-15 16:56:53 +0300

    error handler

--- social_media_backend/app/routes/post.py
+++ social_media_backend/app/routes/post.py
@@ -2,7 +2,6 @@ from datetime import datetime, timezone
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from sqlalchemy.exc import SQLAlchemyError
-
 from ..models.like import Like
 from ..models.post import Post
 from ..models.user import User

88610f2ae9331b8fc1cd2c2e21afc9940d3f3852
eyobScript <eyobbmulugeta@gmail.com>
2025-04-16 01:15:33 +0300

    allow file to exact real extention

--- social_media_backend/app/utils/helpers.py
+++ social_media_backend/app/utils/helpers.py
@@ -1,2 +1,6 @@
 import os
 
+def allowed_file(filename):
+    return ('.' in filename
+            and filename.rsplit('.', 1)[1].lower()
+            in {'jpg', 'jpeg', 'png', 'gif', 'mp4', 'avi', 'mov'})

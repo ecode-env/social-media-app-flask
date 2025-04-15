149fca5dd8092308a555a0c08f4aae5709502f2b
eyobScript <eyobbmulugeta@gmail.com>
2025-04-16 02:12:35 +0300

    import

--- social_media_backend/app/routes/user.py
+++ social_media_backend/app/routes/user.py
@@ -1,5 +1,9 @@
-from flask import Blueprint
-
-
+from flask import Blueprint, jsonify
+from ..models.like import Like
+from ..models.post import Post
+from ..models.user import User
+from ..models.follow import Follow
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from sqlalchemy import desc
 
 users_bp = Blueprint('users',__name__)

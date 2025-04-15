dc7910fc61cccb8ba0eff3d7c3db91beaaa82291
eyobScript <eyobbmulugeta@gmail.com>
2025-04-16 01:11:39 +0300

    import all requires

--- social_media_backend/app/routes/comment.py
+++ social_media_backend/app/routes/comment.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
-
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity, current_user
+from ..models.comment import Comment
+from ..models.CommentLike import CommentLike
+from ..extensions import db
 
 comments_bp = Blueprint('comments', __name__)

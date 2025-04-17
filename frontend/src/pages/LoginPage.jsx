06021b1fb1467e03b9768d30a6a894a9eec94b7a
eyobScript <eyobbmulugeta@gmail.com>
2025-04-17 22:14:09 +0300

    initial state

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import LoginForm from '../components/features/auth/LoginForm';
+
+function LoginPage() {
+  return (
+    <div className="login-page">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default LoginPage;

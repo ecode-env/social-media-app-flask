c2264ead2ddcfb53fbe4c7bcbbef6cfc97543f22
eyobScript <eyobbmulugeta@gmail.com>
2025-05-01 00:59:41 +0300

    login form

--- frontend/src/components/features/auth/LoginForm.jsx
+++ frontend/src/components/features/auth/LoginForm.jsx
@@ -1,20 +1,30 @@
 import '../../../styles/login.css'; // Assumes CSS is in a separate file
 import React, { useState } from 'react';
+import { useAuth } from '../../../hooks/useAuth';
+
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const { login } = useAuth();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Handle login logic here
-    console.log('Email:', email, 'Password:', password);
+    try {
+      await login(email, password);
+      setError('');
+    } catch (err) {
+      console.log(err)
+      setError(err.message);
+    }
   };
 
   return (
       <div className='login-flex-form'>
         <div className="form-container">
       <p className="title">Welcome back</p>
+          {error && <p className="error">{error}</p>}
       <form className="form" onSubmit={handleSubmit}>
         <input
           type="email"
@@ -43,26 +53,11 @@ const LoginForm = () => {
       </form>
       <p className="sign-up-label">
         Don't have an account?{" "}
-        <a href="/signup" className="sign-up-link">
+        <a href="/registor" className="sign-up-link">
           Sign up
         </a>
       </p>
       <div className="buttons-container">
-        <button className="apple-login-button">
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            strokeWidth="0"
-            className="apple-icon"
-            viewBox="0 0 1024 1024"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M747.4 535.7c-.4-68.2 30.5-119.6 92.9-157.5-34.9-50-87.7-77.5-157.3-82.8-65.9-5.2-138 38.4-164.4 38.4-27.9 0-91.7-36.6-141.9-36.6C273.1 298.8 163 379.8 163 544.6c0 48.7 8.9 99 26.7 150.8 23.8 68.2 109.6 235.3 199.1 232.6 46.8-1.1 79.9-33.2 140.8-33.2 59.1 0 89.7 33.2 141.9 33.2 90.3-1.3 167.9-153.2 190.5-221.6-121.1-57.1-114.6-167.2-114.6-170.7zm-105.1-305c50.7-60.2 46.1-115 44.6-134.7-44.8 2.6-96.6 30.5-126.1 64.8-32.5 36.8-51.6 82.3-47.5 133.6 48.4 3.7 92.6-21.2 129-63.7z"></path>
-          </svg>
-          <span>Log in with Apple</span>
-        </button>
         <button className="google-login-button">
           <svg
             stroke="currentColor"

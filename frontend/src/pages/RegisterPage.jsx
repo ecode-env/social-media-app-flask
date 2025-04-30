3e407b4b0099ed1670d967f86dce4c9a5ce0b17d
eyobScript <eyobbmulugeta@gmail.com>
2025-05-01 01:00:05 +0300

    register  page

---
+++ frontend/src/pages/RegisterPage.jsx
@@ -0,0 +1,11 @@
+import RegisterForm from "../components/features/auth/RegisterForm.jsx";
+
+function RegisterPage() {
+  return (
+    <div className="login-page">
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default RegisterPage;

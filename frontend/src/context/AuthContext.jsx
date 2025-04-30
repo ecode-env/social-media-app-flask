d00e989cd4d59836e358e6c320fd6bcb9987c4d1
eyobScript <eyobbmulugeta@gmail.com>
2025-04-30 23:23:57 +0300

    auth context and to access user

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,13 +1,10 @@
 import { createContext, useState, useEffect } from 'react';
 import api from '../services/api';
 
-
 export const AuthContext = createContext();
-
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
-
 
   useEffect(() => {
     const token = localStorage.getItem('token');

fd729974d23bccbf1f858c4a15e5041351b0466b
eyobScript <eyobbmulugeta@gmail.com>
2025-05-03 01:33:43 +0300

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
               <Route path="/register" element={<RegisterPage />} />
             </Routes>
           </main>
+          <Footer />
         </Router>
       </ThemeProvider>
     </AuthProvider>

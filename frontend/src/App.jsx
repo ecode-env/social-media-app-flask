851fad45c26d8a11ba575ac42cd41130b6422b6c
eyobScript <eyobbmulugeta@gmail.com>
2025-05-03 01:33:39 +0300

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/login" element={<LoginPage />} />
-              <Route path="/registor" element={<RegisterPage />} />
+              <Route path="/register" element={<RegisterPage />} />
             </Routes>
           </main>
         </Router>

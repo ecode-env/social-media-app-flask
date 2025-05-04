0f748bd1e6f77361463a3c97a18f9910b61b2d26
eyobScript <eyobbmulugeta@gmail.com>
2025-05-04 20:13:51 +0300

    use post

---
+++ frontend/src/hooks/usePosts.jsx
@@ -0,0 +1,30 @@
+import {useState, useEffect} from "react";
+import { getPosts } from '../services/postService';
+
+export function usePosts() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const data = await getPosts();
+        if (Array.isArray(data)) {
+          setPosts(data);
+        } else {
+          setPosts([]);
+        }
+      } catch (err) {
+        setError(err.message || 'Something went wrong');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+  console.log(posts)
+  return { posts, loading, error };
+}

1ff28864f50574001077467fdaf22077c732850b
eyobScript <eyobbmulugeta@gmail.com>
2025-05-07 17:48:07 +0300

    get comments by post id

--- frontend/src/services/commentService.js
+++ frontend/src/services/commentService.js
@@ -1,2 +1,11 @@
 import api from './api.js'
 
+
+    try {
+    const  res = await api.get('/comments/${PostId}/comments')
+        return res.data
+    }catch (e) {
+        console.log(e);
+        throw e;
+    }
+}

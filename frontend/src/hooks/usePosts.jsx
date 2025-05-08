d0a7b3690910d050798fb4dea6e394fea40dd00c
eyobScript <eyobbmulugeta@gmail.com>
2025-05-08 16:57:43 +0300

    upgradeble

--- frontend/src/hooks/usePosts.jsx
+++ frontend/src/hooks/usePosts.jsx
@@ -1,5 +1,5 @@
 import {useState, useEffect} from "react";
-import { getPosts } from '../services/postService';
+import { getPosts, getPostById } from '../services/postService';
 
 export function usePosts() {
   const [posts, setPosts] = useState([]);
@@ -26,4 +26,25 @@ export function usePosts() {
     fetchPosts();
   }, []);
   return { posts, loading, error };
+}
+
+export function useGetPosts(postId) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [posts, setPosts] = useState([]);
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const data = await getPostById(postId);
+        console.log(data)
+      }catch (e) {
+        console.log(e)
+        throw new  Error('Could not fetch posts', e);
+      }
+    }
+  }, [])
+  return { loading, error };
 }

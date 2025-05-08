043b9e0a606a5d8549e015920f4b993b53b0906f
eyobScript <eyobbmulugeta@gmail.com>
2025-05-08 18:36:38 +0300

    upgradeble

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './components/layout/Footer.jsx'
 import HomePage            from './pages/HomePage.jsx';
 import LoginPage           from './pages/LoginPage';
 import RegisterPage        from "./pages/RegisterPage.jsx";
-import SinglePost from './components/features/posts/SinglePost.jsx'
+import PostDetail from './components/features/posts/PostDetail.jsx'
 import './index.css';
 import './App.css';
 
@@ -21,7 +21,7 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
-              <Route path="/posts/:id" element={<SinglePost />} />
+              <Route path="/posts/:postId" element={<PostDetail />} />
             </Routes>
           </main>
           <Footer />

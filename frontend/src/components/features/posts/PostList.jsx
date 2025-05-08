120650d5b70a8fed19624af1c242c6fcc3ea62c8
eyobScript <eyobbmulugeta@gmail.com>
2025-05-08 12:28:40 +0300

    loading and error

--- frontend/src/components/features/posts/PostList.jsx
+++ frontend/src/components/features/posts/PostList.jsx
@@ -66,6 +66,8 @@ const PostList = () => {
     }
   };
 
+  if (loading) return <div>Loading...</div>;
+  if (error)   return <div>Error: {error}</div>;
 
   return (
     <div className="post-page">

398240ce4f1c40ffa6fa648685cc3368fa8f66db
eyobScript <eyobbmulugeta@gmail.com>
2025-05-08 18:36:42 +0300

    upgradeble

--- frontend/src/components/common/Loader.jsx
+++ frontend/src/components/common/Loader.jsx
@@ -1,88 +1,71 @@
-// Loader.jsx
 import React from 'react';
 import styled from 'styled-components';
 
 const Loader = () => {
     return (
-        <Wrapper>
-            <div className="loader" />
-        </Wrapper>
+        <StyledWrapper>
+            <div className="bg">
+                <div className="loader" />
+            </div>
+        </StyledWrapper>
     );
-};
+}
 
-const Wrapper = styled.div`
-    position: fixed;
-    inset: 0;
-    background-color: var(--bg-color);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 9999;
+const StyledWrapper = styled.div`
+  .bg {
+    padding: 20px;
+    border-radius: 10px;
+    background-color: var(--base-color);
+    --base-color: #212121;
+   /*use your base color*/
+  }
 
-    .loader {
-        width: 60px;
-        height: 40px;
-        position: relative;
-        background-color: var(--primary-color);
+  .loader {
+    width: 60px;
+    height: 40px;
+    position: relative;
+    display: inline-block;
+    background-color: var(--base-color);
+  }
+
+  .loader::before {
+    content: '';
+    left: 0;
+    top: 0;
+    position: absolute;
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    background-color: #FFF;
+    background-image: radial-gradient(circle 8px at 18px 18px, var(--base-color) 100%, transparent 0), radial-gradient(circle 4px at 18px 0px, var(--base-color) 100%, transparent 0), radial-gradient(circle 4px at 0px 18px, var(--base-color) 100%, transparent 0), radial-gradient(circle 4px at 36px 18px, var(--base-color) 100%, transparent 0), radial-gradient(circle 4px at 18px 36px, var(--base-color) 100%, transparent 0), radial-gradient(circle 4px at 30px 5px, var(--base-color) 100%, transparent 0), radial-gradient(circle 4px at 30px 5px, var(--base-color) 100%, transparent 0), radial-gradient(circle 4px at 30px 30px, var(--base-color) 100%, transparent 0), radial-gradient(circle 4px at 5px 30px, var(--base-color) 100%, transparent 0), radial-gradient(circle 4px at 5px 5px, var(--base-color) 100%, transparent 0);
+    background-repeat: no-repeat;
+    box-sizing: border-box;
+    animation: rotationBack 3s linear infinite;
+  }
+
+  .loader::after {
+    content: '';
+    left: 35px;
+    top: 15px;
+    position: absolute;
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    background-color: #FFF;
+    background-image: radial-gradient(circle 5px at 12px 12px, var(--base-color) 100%, transparent 0), radial-gradient(circle 2.5px at 12px 0px, var(--base-color) 100%, transparent 0), radial-gradient(circle 2.5px at 0px 12px, var(--base-color) 100%, transparent 0), radial-gradient(circle 2.5px at 24px 12px, var(--base-color) 100%, transparent 0), radial-gradient(circle 2.5px at 12px 24px, var(--base-color) 100%, transparent 0), radial-gradient(circle 2.5px at 20px 3px, var(--base-color) 100%, transparent 0), radial-gradient(circle 2.5px at 20px 3px, var(--base-color) 100%, transparent 0), radial-gradient(circle 2.5px at 20px 20px, var(--base-color) 100%, transparent 0), radial-gradient(circle 2.5px at 3px 20px, var(--base-color) 100%, transparent 0), radial-gradient(circle 2.5px at 3px 3px, var(--base-color) 100%, transparent 0);
+    background-repeat: no-repeat;
+    box-sizing: border-box;
+    animation: rotationBack 4s linear infinite reverse;
+  }
+
+  @keyframes rotationBack {
+    0% {
+      transform: rotate(0deg);
     }
 
-    .loader::before {
-        content: '';
-        position: absolute;
-        width: 36px;
-        height: 36px;
-        border-radius: 50%;
-        background-color: #FFF;
-        background-image:
-                radial-gradient(circle 8px at 18px 18px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 4px at 18px 0px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 4px at 0px 18px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 4px at 36px 18px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 4px at 18px 36px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 4px at 30px 5px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 4px at 30px 30px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 4px at 5px 30px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 4px at 5px 5px, var(--base-color) 100%, transparent 0);
-        background-repeat: no-repeat;
-        box-sizing: border-box;
-        animation: rotate 3s linear infinite;
+    100% {
+      transform: rotate(-360deg);
     }
-
-    .loader::after {
-        content: '';
-        position: absolute;
-        left: 35px;
-        top: 15px;
-        width: 24px;
-        height: 24px;
-        border-radius: 50%;
-        background-color: #FFF;
-        background-image:
-                radial-gradient(circle 5px at 12px 12px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 2.5px at 12px 0px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 2.5px at 0px 12px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 2.5px at 24px 12px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 2.5px at 12px 24px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 2.5px at 20px 3px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 2.5px at 20px 20px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 2.5px at 3px 20px, var(--base-color) 100%, transparent 0),
-                radial-gradient(circle 2.5px at 3px 3px, var(--base-color) 100%, transparent 0);
-        background-repeat: no-repeat;
-        box-sizing: border-box;
-        animation: rotateReverse 4s linear infinite;
-    }
-
-    @keyframes rotate {
-        to {
-            transform: rotate(-360deg);
-        }
-    }
-
-    @keyframes rotateReverse {
-        to {
-            transform: rotate(360deg);
-        }
-    }
-`;
+  }`;
 
 export default Loader;

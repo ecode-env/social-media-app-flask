5cfafb4c04343879053addc6f065e3b032fec2bd
eyobScript <eyobbmulugeta@gmail.com>
2025-05-24 23:10:15 +0300

    user context

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,33 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { getCurrentUser } from "../services/authService.js";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        try {
+            const currentUser = getCurrentUser();
+            setUser(currentUser);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            setLoading(false);
+        }
+    }, []);
+
+    const logout = () => {
+        localStorage.removeItem("user");
+        setUser(null);
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, loading, logout, isAuthenticated: !!user }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);

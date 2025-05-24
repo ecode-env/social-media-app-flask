ff4139ec8f03416b6fdfcb43724e2b88bc0496f6
eyobScript <eyobbmulugeta@gmail.com>
2025-05-24 23:27:26 +0300

    to load and make easy the data we fetched

---
+++ frontend/src/hooks/usePosts.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { fetchPosts } from "../services/postService.js";
+
+const usePosts = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const loadPosts = async () => {
+            try {
+                const getPosts = await fetchPosts();
+                setPosts(getPosts);
+            } catch (e) {
+                setError(e.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        loadPosts();
+    }, []);
+
+    return { loading, posts, error };
+};
+
+export default usePosts;

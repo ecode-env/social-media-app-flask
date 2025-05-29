a793545e0bfac80b8ae408d3ec9f43b60f7b0313
eyobScript <eyobbmulugeta@gmail.com>
2025-05-29 03:00:36 +0300

    import btn and context

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import Header from '../components/common/Header.jsx';
 import { Bell, MessageSquare, UserCircle, PlusSquare, Home, LogIn } from 'lucide-react';
 import './MainLayout.css';
+import Button from "../components/common/Button.jsx";
+import { useAuth } from '../context/AuthContext.jsx'
 
 const MainLayout = () => {
   return (

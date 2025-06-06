9accf2e0859cfb56973803fbb532788e0b4bd622
eyobScript <eyobbmulugeta@gmail.com>
2025-06-06 11:51:55 +0300

    rollback optimistic update

--- frontend/src/components/posts/PostList.jsx
+++ frontend/src/components/posts/PostList.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import {Heart, MessageSquare,SendHorizontal,Bookmark} from 'lucide-react';
 import './PostList.css';
-import {fetchPosts, likePost} from '../../services/postService.js';
+import { likePost} from '../../services/postService.js';
 import { useAuth } from '../../context/AuthContext.jsx';
 import usePosts from "../../hooks/usePosts.jsx";
 

8e1d815b5786bd3132b41bf4f8aa50e6b12e0d82
eyobScript <eyobbmulugeta@gmail.com>
2025-06-07 20:13:50 +0300

    hook

--- social_media_backend/app/routes/user.py
+++ social_media_backend/app/routes/user.py
@@ -9,8 +9,8 @@ from sqlalchemy import desc
 users_bp = Blueprint('users',__name__)
 
 
-@users_bp.route('/<username>/profile', methods=['GET'])
-@jwt_required()
+@users_bp.route('/<username>', methods=['GET'])
+@jwt_required(optional=True)
 def get_user_profile(username):
     user = User.query.filter_by(username=username).first_or_404()
     current_user_id = get_jwt_identity()
@@ -26,16 +26,33 @@ def get_user_profile(username):
     # Liked post IDs (only for profile user's posts)
     liked_post_ids = []
     if current_user_id:
-        liked_post_ids = [like.post_id for like in Like.query.filter_by(user_id=current_user_id)
-        .join(Post).filter(Post.user_id == user.id).all()]
+        liked_post_ids = [
+            like.post_id for like in Like.query.filter_by(user_id=current_user_id)
+            .join(Post).filter(Post.user_id == user.id).all()
+        ]
 
-    # Recent posts with ordering
-    recent_posts = [post.to_json() for post in Post.query.filter_by(user_id=user.id)
-    .order_by(desc(Post.created_at)).limit(5).all()]
+    # Recent posts
+    recent_posts = [
+        post.to_json() for post in Post.query
+        .filter_by(user_id=user.id)
+        .order_by(desc(Post.created_at))
+        .limit(5)
+        .all()
+    ]
 
-    # Followers and following (limited)
-    followers = [f.follower.to_json() for f in Follow.query.filter_by(following_id=user.id).limit(5).all()]
-    following = [f.following.to_json() for f in Follow.query.filter_by(follower_id=user.id).limit(5).all()]
+    # Followers and following
+    followers = [
+        f.follower.to_json() for f in Follow.query
+        .filter_by(following_id=user.id)
+        .limit(5)
+        .all()
+    ]
+    following = [
+        f.following.to_json() for f in Follow.query
+        .filter_by(follower_id=user.id)
+        .limit(5)
+        .all()
+    ]
 
     return jsonify({
         "user": user.to_json(),

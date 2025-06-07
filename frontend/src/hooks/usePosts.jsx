c7038088582bcaf5cb36171f45376728e3b53da3
eyobScript <eyobbmulugeta@gmail.com>
2025-06-07 22:28:30 +0300

    hook

--- frontend/src/hooks/usePosts.jsx
+++ frontend/src/hooks/usePosts.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { fetchPosts } from "../services/postService.js";
 
 const usePosts = () => {

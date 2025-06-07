ab549490dea0ce9f0fa170586a29011e3e4367af
eyobScript <eyobbmulugeta@gmail.com>
2025-06-07 16:56:57 +0300

    to add comment

--- frontend/src/services/commentService.js
+++ frontend/src/services/commentService.js
@@ -9,3 +9,14 @@ export const getCommentById = async (postId) => {
         throw new Error('Could not get commentById');
     }
 }
+
+// Add a comment to a post
+export const addComment = async (postId, comment) => {
+    try {
+        const response = await api.post(`/comments/${postId}/create-comment`, comment);
+        return response.data;
+
+    } catch (error) {
+        throw error;
+    }
+};

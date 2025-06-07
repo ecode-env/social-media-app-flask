f0c1ed5d1349019e3b3c6e62dc0c96ae75a951cd
eyobScript <eyobbmulugeta@gmail.com>
2025-06-07 22:31:10 +0300

    hook

--- frontend/src/hooks/usePosts.jsx
+++ frontend/src/hooks/usePosts.jsx
@@ -9,6 +9,7 @@ const usePosts = ({ filterByUser = false, userId = null }) => {
     const [error, setError] = useState(null);
     const { user } = useAuth();
 
+
     useEffect(() => {
         const loadPosts = async () => {
             try {

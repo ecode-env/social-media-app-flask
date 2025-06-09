9e01f19650563f3c71c2d2ea5417dc340159b88f
eyobScript <eyobbmulugeta@gmail.com>
2025-06-09 23:49:36 +0300

    hooks

--- frontend/src/pages/CreatePostPage.jsx
+++ frontend/src/pages/CreatePostPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Type, FileText } from 'lucide-react';
 import Button from '../components/common/Button';

35e52be51f69648350aaabbb0dab20a5dcf5b5a3
eyobScript <eyobbmulugeta@gmail.com>
2025-06-09 23:48:15 +0300

    save token

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,32 +1,16 @@
 import api from './api.js';
 
-// Define mock users for local development
-const mockUsers = [
-  {
-    id: 1,
-    username: 'johndoe',
-    email: 'john@example.com',
-    password: 'password123', // In a real app, never store plain text passwords!
-    f_name: 'John',
-    l_name: 'Doe',
-    profile_picture_url: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg'
-  },
-  {
-    id: 2,
-    username: 'janedoe',
-    email: 'jane@example.com',
-    password: 'password123',
-    f_name: 'Jane',
-    l_name: 'Doe',
-    profile_picture_url: 'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg'
-  }
-];
-
 // Login function
 export const login = async (email, password) => {
   try {
     const response = await api.post('auth/login', {email, password});
-    return response.data;
+
+    const { user, access_token } = response.data;
+
+    // Store user data and token in localStorage
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('token', access_token);
+    return  response.data;
   } catch (e) {
     throw e;
   }

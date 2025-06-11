13a51890e36f17ab5ab2b7616d4c496061b2994f
eyobScript <eyobbmulugeta@gmail.com>
2025-06-11 23:51:15 +0300

    login

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -30,3 +30,8 @@ export const getCurrentUser = () => {
   const user = localStorage.getItem('user');
   return user ? JSON.parse(user) : null;
 };
+
+export const logout = () => {
+  localStorage.removeItem('user');
+  localStorage.removeItem('token');
+};

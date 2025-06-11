0587bfb69cae3fae7c1f5faeaa3189595a49f814
eyobScript <eyobbmulugeta@gmail.com>
2025-06-11 23:51:02 +0300

    login

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -17,8 +17,6 @@ export const register = async (userData) => {
   try {
     const response = await api.post('/auth/register', userData);
     const { user, access_token } = response.data;
-
-    // Store user data and token in localStorage
     localStorage.setItem('user', JSON.stringify(user));
     localStorage.setItem('token', access_token);
 

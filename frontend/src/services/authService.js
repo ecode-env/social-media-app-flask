03546746276e5158208111f0499ab014e503515c
eyobScript <eyobbmulugeta@gmail.com>
2025-06-11 23:51:12 +0300

    login

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -26,9 +26,6 @@ export const register = async (userData) => {
   }
 };
 
-
-
-// Get current user function
 export const getCurrentUser = () => {
   const user = localStorage.getItem('user');
   return user ? JSON.parse(user) : null;

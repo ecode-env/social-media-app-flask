e4f2294a30c13bdd0ba2a5f6c0b968183362e8eb
eyobScript <eyobbmulugeta@gmail.com>
2025-06-11 17:05:12 +0300

    massage

--- frontend/src/pages/PostDetailPage.jsx
+++ frontend/src/pages/PostDetailPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import { Heart, Send } from 'lucide-react';
 import Button from '../components/common/Button';
 import './PostDetailPage.css';

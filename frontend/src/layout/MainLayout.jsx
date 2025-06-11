99d939be5a1962d4ae2720955c79547ee69c71f5
eyobScript <eyobbmulugeta@gmail.com>
2025-06-11 23:54:22 +0300

    update

--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -13,53 +13,52 @@ const MainLayout = () => {
     const handleRegister = () => navigate('/register');
     const handleLogin = () => navigate('/login');
 
-  return (
-      <div className="layout">
-        <Header />
-
-        <div className="layout-container">
-          <aside className={`sidebar ${hideSidebar ? 'hidden' : ''} left-sidebar`}>
-            <div className="sidebar-content">
-              <h3>Navigation</h3>
-              <div className="sidebar-nav">
-                {currentUser ? (
-                    <div className="user-sidebar-box">
-                      <img
-                          src={currentUser.profile_picture_url || 'https://i.pinimg.com/736x/c0/74/9b/c0749b7cc401421662ae901ec8f9f660.jpg'}
-                          alt="User"
-                          className="user-avatar"
-                      />
-                      <div className="user-space"></div>
-                      <p className="username">@{currentUser.username}</p>
-                      <div className="user-follow">
-                        <a>
-                          <strong>{currentUser.following || 0}</strong> Following
-                        </a>
-                        <a><strong>{currentUser.followers || 0}</strong> Followers</a>
-                      </div>
+    return (
+        <div className="layout">
+            <Header />
+            <div className="layout-container">
+                <aside className="sidebar left-sidebar">
+                    <div className="sidebar-content">
+                        <h3>Navigation</h3>
+                        <div className="sidebar-nav">
+                            {user ? (
+                                <div className="user-sidebar-box">
+                                    <img
+                                        src={user.profile_picture_url || 'https://i.pinimg.com/736x/c0/74/9b/c0749b7cc401421662ae901ec8f9f660.jpg'}
+                                        alt="User"
+                                        className="user-avatar"
+                                    />
+                                    <div className="user-space"></div>
+                                    <p className="username">@{user.username}</p>
+                                    <div className="user-follow">
+                                        <a>
+                                            <strong>{user.following || 0}</strong> Following
+                                        </a>
+                                        <a>
+                                            <strong>{user.followers || 0}</strong> Followers
+                                        </a>
+                                    </div>
+                                </div>
+                            ) : (
+                                <>
+                                    <div className='sidebar-btn'>
+                                        <Button variant="primary-light" onClick={handleLogin}>Login</Button>
+                                        <Button variant="primary" onClick={handleRegister}>Register</Button>
+                                    </div>
+                                    <div className='sidebar-text'>
+                                        <h6>Please login</h6>
+                                    </div>
+                                </>
+                            )}
+                        </div>
                     </div>
-                ) : (
-                    <>
-                      <div className='sidebar-btn'>
-                          <Button variant="primary-light" onClick={handleLogin}>Login</Button>
-                          <Button variant="primary" onClick={handleRegister}>Register</Button>
-                      </div>
-                      <div className='sidebar-text'>
-                        <h6 >Please logging</h6>
-                      </div>
-                    </>
-
-                )}
-              </div>
+                </aside>
+                <main className="main-content">
+                    <Outlet />
+                </main>
             </div>
-          </aside>
-
-          <main className="main-content">
-            <Outlet />
-          </main>
         </div>
-      </div>
-  );
+    );
 };
 
 export default MainLayout;
